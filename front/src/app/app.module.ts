3bd121258c2d069cb123470d1f5bb6651d479b1f
jumpingrock <kennethyeongky@gmail.com>
2020-05-20 18:13:28 +0800

    created structure of project

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReportFormComponent } from './report-form/report-form.component';
+import { ApprovalFormComponent } from './approval-form/approval-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ReportFormComponent,
+    ApprovalFormComponent
   ],
   imports: [
     BrowserModule
---
+++ front/src/app/approval-form/approval-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-approval-form',
+  templateUrl: './approval-form.component.html',
+  styleUrls: ['./approval-form.component.css']
+})
+export class ApprovalFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
