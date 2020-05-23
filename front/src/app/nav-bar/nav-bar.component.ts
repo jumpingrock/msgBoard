1f0f64b5f06691fc914a2bd9dff23196a068c4c2
jumpingrock <kennethyeongky@gmail.com>
2020-05-24 03:14:01 +0800

    slight refactor

--- front/src/app/nav-bar/nav-bar.component.ts
+++ front/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from '../report.service';
+import { Subscription } from 'rxjs';
+import { userForm } from '../app.model';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,13 +12,20 @@ export class NavBarComponent implements OnInit {
   collapsed: boolean = true;
   showCreateReport: boolean = true;
   showApproveReport: boolean = true;
-  constructor() { }
+  loginSub: Subscription;
+  userLoggedIn: boolean = false;
+  constructor(private reportService: ReportService) { }
 
   ngOnInit(): void {
+    this.loginSub = this.reportService.userLoginListener()
+      .subscribe((user: userForm) => {
+        this.userLoggedIn = true;
+    })
   }
   onShowCreateReport () {
     this.showApproveReport = false;
     this.showCreateReport = true;
+
   }
   onShowApproveReport () {
     this.showApproveReport = true;
